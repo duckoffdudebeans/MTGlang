efeb5f79e9c841f13162fd8021664ff9055285a0
duckoffdudebeans <zeke81257@outlook.com>
2026-09-14 16:09:51 -0400

    rewrite memory 1

--- mtglang 2/helpfuls.hpp
+++ mtglang 2/helpfuls.hpp
@@ -2,18 +2,54 @@
 #include <iostream>
 #include <vector>
 #include <list>
+
+
 template<typename datatype, typename b, typename a, typename c>
+
 class utils
 {
+    int adresser = 0;
+
+
     class variable
     {
         std::string name;
         datatype data;
-        int adress;
+
+        int adress = adresser;
     };
-    std::vector<variable> data;
-    std::vector<memorylist> mem = [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,];
-    datatype variablehandle (std::string name,bool write,int adress,datatype write)
+
+
+    std::vector<variable> mem;
+
+
+    int writer (int adress, datatype datum,bool newvar)
     {
+        return 0;
+    }
+
+
+    datatype variablehandle (std::string name,bool write,datatype data)
+    {
+        if (write = true)
+        {
+            for (int i = 0; i < mem.size(); i++)
+            {
+                if ( mem[i].name == name )
+                {
+                    writer(datum= data adress = i);
+                }
+
+                if ( i == (mem.size() - 1))
+                {
+                    if (writer(datum= data, adress = adresser, newvar = true)==1)
+                    {
+                        return "VARWRITE<SUCCESS>"
+                    }
+
+                    adresser++;
+                }
+            }
+        }
     }
 };
